cd22ee947380ab8e4bc568a87cba03de80a15afc
Vladimir <vladimirvukovic98@hotmail.rs>
2020-11-09 15:38:11 +0100

    backend without jwt

---
+++ django/todo_be/todo_api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
+
+router.register(r'todos', views.ToDoViewSer, basename="todo")
+router.register(r'users', views.UserViewSet, basename='user')
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+]

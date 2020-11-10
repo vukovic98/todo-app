bf9cea67406623da50c75ee66486182e1d9593c2
vukovic98 <45330563+vukovic98@users.noreply.github.com>
2020-11-10 09:09:57 +0100

    Update django/todo_be/todo_api/serializers.py
    Co-authored-by: Dragan <dragan1176@gmail.com>

--- django/todo_be/todo_api/serializers.py
+++ django/todo_be/todo_api/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import ToDo
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'email', 'password')
